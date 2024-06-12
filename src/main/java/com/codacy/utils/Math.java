73441f648f4ea9e757ea3ce011c84eba476415ed
Joao Machado <13315199+machadoit@users.noreply.github.com>
2024-06-12 11:06:54 +0100

    Update Math.java

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -26,6 +26,7 @@ public class Math {
             System.out.println("I am covered!");
             System.out.println("I am covered!");
             System.out.println("I am covered!");
+            System.out.println("I am covered!");
             return x + y;
         }
     }
