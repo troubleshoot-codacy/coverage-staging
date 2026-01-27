87cb4aaf3c3749865ea1613e326e79712f2b9dcf
Francisco Azevedo <franciscoovazevedo@gmail.com>
2026-01-27 12:06:27 +0000

    Revert "Revert "new commit""

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -28,7 +28,6 @@ public class Math {
             System.out.println("I am covered!");
             System.out.println("I am covered!");
             System.out.println("I am covered!");
-            System.out.println("I am covered!");
             return x + y;
         }
     }
